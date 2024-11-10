2f9aa5a5e28536db21e97821cff29de5e67b34c1
testai-123 <151430801+testai-123@users.noreply.github.com>
2024-11-10 18:54:04 +0530

    Update DMV5_AirQuality_Viz.py

--- DMV5_AirQuality_Viz.py
+++ DMV5_AirQuality_Viz.py
@@ -38,16 +38,36 @@ pollutants = ['PM2.5', 'PM10', 'CO']
 
 # Create separate line plots for each pollutant
 plt.figure(figsize=(12, 8))
-for pollutant in pollutants:
-    plt.plot(cleaned_data['Date'], cleaned_data[pollutant], label=pollutant)
-
+plt.plot(cleaned_data['Date'], cleaned_data['PM2.5'], label=pollutant)
 plt.xlabel('Date')
 plt.ylabel('Concentration')
-plt.title('Pollutant Trends Over Time (Cleaned Data)')
+plt.title('PM2.5 Trends Over Time (Cleaned Data)')
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+plt.figure(figsize=(12, 8))
+plt.plot(cleaned_data['Date'], cleaned_data['PM10'], label=pollutant)
+plt.xlabel('Date')
+plt.ylabel('Concentration')
+plt.title('PM10 Trends Over Time (Cleaned Data)')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(12, 8))
+plt.plot(cleaned_data['Date'], cleaned_data['CO'], label=pollutant)
+plt.xlabel('Date')
+plt.ylabel('Concentration')
+plt.title('CO Trends Over Time (Cleaned Data)')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+
 
 
 # Group the data by 'Date' and calculate the mean AQI
@@ -77,4 +97,32 @@ plt.title('Violin Plot: Distribution of AQI for PM2.5')
 plt.tight_layout()
 plt.show()
 
+plt.figure(figsize=(12, 6))
+sns.violinplot(data=cleaned_data, x='AQI', y='PM10', palette='Set2')
+plt.xlabel('AQI')
+plt.ylabel('PM10')
+plt.title('Violin Plot: Distribution of AQI for PM2.5')
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(12, 6))
+sns.violinplot(data=cleaned_data, x='AQI', y='CO', palette='Set2')
+plt.xlabel('AQI')
+plt.ylabel('CO')
+plt.title('Violin Plot: Distribution of AQI for PM2.5')
+plt.tight_layout()
+plt.show()
+
 # Similar violin plots for PM10 and CO...
+
+x = df['PM2.5']
+y = df['CO']  # Represent 'PM10' using bubble size
+aqi_values = df['AQI']
+bubble_size = df['PM10'] 
+
+plt.figure(figsize=(12, 8))
+plt.scatter(x, y, s = bubble_size, c=aqi_values, cmap='coolwarm', alpha=0.7)
+plt.colorbar(label='AQI')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
