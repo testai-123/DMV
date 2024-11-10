77e2755f9851bbe1150cfe2cad02007977644cb3
testai-123 <151430801+testai-123@users.noreply.github.com>
2024-11-10 23:49:03 +0530

    Update DMV4_RealEstate.py

--- DMV4_RealEstate.py
+++ DMV4_RealEstate.py
@@ -2,7 +2,7 @@ import pandas as pd
 from scipy import stats
 
 # Load dataset
-df = pd.read_csv("/content/RealEstate_price.csv")
+df = pd.read_csv("C:\\Users\\aksha\\Downloads\\DMV4.csv")
 
 # Clean column names (spaces and special characters removed)
 df.columns = df.columns.str.replace(' ', '_').str.replace(r'\W', '', regex=True)
@@ -29,3 +29,11 @@ df.loc[outliers.index, 'Price'] = df['Price'].median()
 
 # Final dataset view after cleaning
 print(df['Price'])
+
+avg_price_by_neighborhood = df.groupby('Neighborhood')['Price'].mean()
+print("Average Sale Price by Neighborhood:")
+print(avg_price_by_neighborhood)
+
+# Final dataset after cleaning and transformations
+print("Final Dataset after Cleaning and Transformations:")
+print(df.head())
